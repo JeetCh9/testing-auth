6465c549ff042d65fcbc5e8f6d216bac5e0c0ada
Jeetch8 <jeetkumar0898@gmail.com>
2022-09-08 18:09:20 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const sendRequest = () => {
+    axios
+      .post("http://localhost:5000/api/v1/auth/login", {
+        email: "testing1@gmail.com",
+        password: "testingPass",
+      })
+      .then((res) => console.log(res.data));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => sendRequest()}>Send Request</button>
     </div>
   );
 }
